cc77b349e75e00022ab27c45ba6114f4fb3ca4b1
pjo901018 <pjo901018@korea.ac.kr>
2017-03-02 03:28:23 +0900

    Load / 라벨 numpy로 변환 추가

--- load/converter.py
+++ load/converter.py
@@ -1,6 +1,16 @@
 import numpy
 
 
+def convert_data_to_numpy(images, labels):
+    images = convert_images_to_numpy(images)
+    labels = convert_labels_to_numpy(labels)
+    return images, labels
+
+def convert_labels_to_numpy(labels):
+    np_labels = numpy.array(labels, dtype=int)
+
+    return np_labels
+
 def convert_images_to_numpy(images):
     arr = numpy.empty([len(images), 48, 28], dtype=float) # todo 28,48 -> flexible
     for idx, image in enumerate(images):
