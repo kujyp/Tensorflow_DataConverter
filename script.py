06e88973c3007a0c6591c26c0a2fcbbe9569ec36
pjo901018 <pjo901018@korea.ac.kr>
2017-03-01 20:11:07 +0900

    Visualize폴더 / 이미지 파일 사이즈측정

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
 from load.loadfile import load_allimage
+from visualize.visualize_pillow import smallest_size, average_size
 
 rootdir = "data"
 images = load_allimage(rootdir)
+print(len(images))
+print(smallest_size(images))
+print(average_size(images))
 
-images[50].show()
+#images = normalize(images)
+#images = convert_images_to_numpy(images)
+
