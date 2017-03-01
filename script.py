559a2c831590c6fc03d6708816b016729a28edc8
pjo901018 <pjo901018@korea.ac.kr>
2017-03-01 20:30:44 +0900

    Process폴더 / gray scale, resize구현 Visualizer폴더 / 파일이름 visualizer_~로 변경

--- script.py
+++ script.py
@@ -1,12 +1,11 @@
 from load.loadfile import load_allimage
-from visualize.visualize_pillow import smallest_size, average_size
+from visualize.visualizer_pillow import smallest_size, average_size
+from process.normalizer_pillow import normalize_images
 
 rootdir = "data"
 images = load_allimage(rootdir)
-print(len(images))
-print(smallest_size(images))
-print(average_size(images))
-
-#images = normalize(images)
+#print(len(images))
+#print(smallest_size(images))
+#print(average_size(images))
+images = normalize_images(images)
 #images = convert_images_to_numpy(images)
-
